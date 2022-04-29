0ea13b5988f68c26f31c1360b9cb0cf10eaeb44e
INSAN <caninsan48@gmail.com>
2022-04-29 14:57:27 +0700

    penambahan index

--- resources/views/items/sidebar_admin.blade.php
+++ resources/views/items/sidebar_admin.blade.php
@@ -97,7 +97,7 @@
 				</li><li>
 					<a href='?module=JS Arrays'><i class='fa fa-angle-double-right'></i>JS Arrays[ GALIH ADI PRATAMA]</a>
 				</li><li>
-					<a href='?module=JS Array Methods'><i class='fa fa-angle-double-right'>	   </i>JS Array Methods[ 		]</a>
+					<a href="{{ route('jsinsan') }}"><i class='fa fa-angle-double-right'>	   </i>JS insan[ insan		]</a>
 				</li><li>
 					<a href='?module=JS Array Sort'><i class='fa fa-angle-double-right'>		  </i>JS Array Sort[	]</a>
 				</li><li>
